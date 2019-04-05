2f28d4c42900b0ac35fd1fde81ad06c993fdf7e5
Jun Tomii <morning.azure@gmail.com>
2019-04-05 11:40:00 +0900

    Initial commit of SupportVectorMachine. Empty functions and some implementation.

---
+++ SupportVectorMachine.py
@@ -0,0 +1,43 @@
+import sys
+
+def SelectSupportVector(data):
+    supportVector = data
+    return supportVector
+
+def MaximumMargin(supportVectors):
+    weight = []
+    return weight
+
+def DotProduct(vector1, vector2):
+    if len(vector1) != len(vector2):
+        print('Error: Vector lengthes dont match.', file=sys.stderr)
+        sys.exit(1)
+    res = 0
+    for scalar1, scalar2 in zip(vector1, vector2):
+        res = res + scalar1 * scalar2
+    return res
+
+def SupportVectorMachine(data, weight, train=True):
+    if train:
+        supportVectors = SelectSupportVector(data)
+        weight = MaximumMargin(supportVectors)
+        return weight
+    else:
+        taggedData = []
+        for datumVector in data:
+            datumVector.append(1)
+            selector = DotProduct(datumVector, weight)
+            tag = selector > 0
+            taggedData.append(tag)
+        return taggedData
+
+def main():
+    dataForTraining = []
+    dataForTest = []
+    weight = []
+
+    weight = SupportVectorMachine(dataForTraining, weight, train=True)
+    taggedData = SupportVectorMachine(dataForTest, weight, train=False)
+
+if __name__ == '__main__':
+    main()
