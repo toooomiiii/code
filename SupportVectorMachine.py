9cfc63d418c81b6ce01e98e9d4b07256969ce118
Jun Tomii <morning.azure@gmail.com>
2019-04-09 07:16:06 +0900

    addition of data and refinement of role of functions.

--- SupportVectorMachine.py
+++ SupportVectorMachine.py
@@ -1,7 +1,56 @@
 import sys
+import math
+import numpy as np
+from scipy import optimize
+from sklearn.datasets import load_iris
 
-def MaximumMargin(data):
-    weight = []
+def MakeIrisDataset(irisDataset):
+    data = irisDataset.data
+    label = irisDataset.target
+    dataFalse = data[label == 0]
+    dataTrue = data[label == 1]
+
+    numOfNineFalse = int(np.ceil(0.5 * len(dataFalse)))
+    numOfNineTrue = int(np.ceil(0.5 * len(dataTrue)))
+
+    dataForTraining = [dataFalse[:numOfNineFalse], dataTrue[:numOfNineFalse]]
+    dataForTest = [dataFalse[numOfNineFalse:], dataTrue[numOfNineTrue:]]
+    return dataForTraining, dataForTest
+
+def CalculateMargin(weight, sv):
+    margin = CalculateDistance(weight, sv[0]) + CalculateDistance(weight, sv[1])
+    return margin
+
+def CalculateDistance(weight, datum):
+    # len(weight) + 1 == len(datum)
+    distance = np.abs(DotProduct(weight[1:], datum) + weight[0]) / DotProduct(weight[1:], weight[1:])
+    return distance
+
+def MaximumMargin(data, initialWeight):
+    dataClassTrue = data[0]
+    dataClassFalse = data[1]
+    
+    weight = initialWeight
+    while True:
+        margins = [0, 1]
+        marginCandidatesTrue = [CalculateDistance(weight, datum) for datum in dataClassTrue]
+        marginCandidatesFalse = [CalculateDistance(weight, datum) for datum in dataClassFalse]
+
+        # choose support vectors which are nearest data samples to the separation plane.
+        [supportVectorTrue, indxTrue] = min(marginCandidatesTrue)
+        [supportVectorFalse, indxFalse] = min(marginCandidatesFalse)
+
+        sv = [dataClassTrue(indxTrue), dataClassFalse(indxFalse)]
+
+        # maximize margin in respect of weight.
+        [mgn, weight] = Maximize(weight, sv)
+
+        margins[1] = margins[0]
+        margins[0] = CalculateMargin(weight, sv)
+
+        if margins[0] - margins[1] < 1:
+            break
+
     return weight
 
 def DotProduct(vector1, vector2):
@@ -15,8 +64,8 @@ def DotProduct(vector1, vector2):
 
 def SupportVectorMachine(data, weight, train=True):
     if train:
-        weight = MaximumMargin(data)
-        return weight
+        resultWeight = MaximumMargin(data, weight)
+        return resultWeight
     else:
         taggedData = []
         for datumVector in data:
@@ -27,9 +76,10 @@ def SupportVectorMachine(data, weight, train=True):
         return taggedData
 
 def main():
-    dataForTraining = []
-    dataForTest = []
-    weight = []
+    iris_dataset = load_iris()
+    [dataForTraining, dataForTest] = MakeIrisDataset(iris_dataset)
+    dataSize = np.size(dataForTraining[0][0])
+    weight = np.random.rand(dataSize + 1)
 
     weight = SupportVectorMachine(dataForTraining, weight, train=True)
     taggedData = SupportVectorMachine(dataForTest, weight, train=False)
